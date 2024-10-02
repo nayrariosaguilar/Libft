ef2d1a278c9e0f6f1de8a66a92bb083774b9b8b2
Nayra Rios <nayrios2004@gmail.com>
2024-10-02 11:50:43 +0200

    MasFuncioens

---
+++ ft_atoi.c
@@ -0,0 +1,22 @@
+#include "libft.h"
+
+int ft_atoi(const char *str)
+{
+    int res;
+    int sign;
+
+    res = 0;
+    sign = 1;
+    while (*str == 32 || (*str >= 9 && *str <= 13))
+        str++;
+    if (*str == '-')
+        sign *= -1;
+    if (*str == '-' || *str == '+')
+        str++;
+    while (*str >= '0' && *str <= '9')
+    {
+        res = res * 10 + *str - '0';
+        str++;
+    }
+    return (res * sign);
+}
